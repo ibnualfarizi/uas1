b1f23db99976351c0eac849a6e643a85554305f6
ibnualfarizi <alfariziibnu14@gmail.comm>
2020-01-19 11:11:29 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project UAS Bahasa Pemrograman
+silakan tuliskan langkah-langkah disertai screenshotnya
+# uas
+# uas1
---
+++ main.py
@@ -0,0 +1,12 @@
+from core.baseapp import BaseApp
+from view.view_book impor *
+class MainApp(BaseApp):
+    def __init__(self):
+        self.books = []
+        def list_book(self):
+            self.list_book()
+
+
+if__name__=="__main__"
+app = MainApp()
+self.run
